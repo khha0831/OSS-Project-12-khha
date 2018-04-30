4a0fb7cd18f698c43592f09c3c0a00eadb841e4a
436-01\user <khha777@gmail.com>
2018-04-30 10:14:04 +0900

    코드수정

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main() {
-  printf("First");
-  printf("Second");
-  
+int main()
+{
+  printf("First OSS Code\n");
+  printf("Second OSS Code\n");
+  printf("마스터브랜치에 코드를 추가하였음")
   return 0;
-
 }
